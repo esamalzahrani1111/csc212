65a46465bacaa490131db4b308e8769ca74f1ed3
esamalzahrani1111 <444100946@student.ksu.edu.sa>
2024-11-09 16:20:19 +0300

    implemented bst tree

--- src/BSTNode.java
+++ src/BSTNode.java
@@ -1,3 +1,18 @@
-public class BSTNode {
-
+public class BSTNode <T> {
+	public int key;
+	public T data;
+	public BSTNode<T> left, right;
+	
+	public BSTNode(int k, T val) {
+		key = k;
+		data = val;
+		left = right = null;
+	}
+	
+	public BSTNode(int k, T val, BSTNode<T> l, BSTNode<T> r) {
+		key = k;
+		data = val;
+		left = l;
+		right = r;
+	}
 }
