2e4bf65990967f48cc974bfa339bd5aba8e21853
mhpolymath <mhpolymath@gmail.com>
2024-11-13 19:31:45 +0300

    reformatted the structure of BSTNode
    made the BSTNode work with DocFreq instead of docId alone.
    this is done to include the frequency of the term in the
    document alongside the document id.
    used for ranking documents in non-Boolean queries
    
    previous inverted index format:
    	term1 -> [doc_x, doc_y, ...]
    	term2 -> [doc_a, ...]
    	...
    
    what to implement: (for ranking)
    	term1 -> [doc_x:freq_x, doc_y:freq_y, ...]
    	term2 -> [doc_a:freq_a, ...]
    	...

--- src/BSTNode.java
+++ src/BSTNode.java
@@ -1,18 +1,43 @@
+/*
+current inverted index format:
+	term1 -> [doc_x, doc_y, ...]
+	term2 -> [doc_a, ...]
+	...
+
+what to implement: (for ranking)
+	term1 -> [doc_x:freq_x, doc_y:freq_y, ...]
+	term2 -> [doc_a:freq_a, ...]
+	...
+ */
+
+// Node class for storing data: docId and frequency of term
+class DocFrequency<T extends Comparable<T>> {
+	public T docId;
+	public int frequency;
+	public DocFrequency<T> next;
+	
+	public DocFrequency(T docId) {
+        this.docId = docId;
+        this.frequency = 1; // Initialize frequency to 1 when first encountered
+        this.next = null;
+    }
+}
+
 public class BSTNode <T extends Comparable<T>> {
 	public String key;
-	public Node<T> data;
+	public DocFrequency<T> data;
 	public BSTNode<T> left, right;
 	
 	/** Creates a new instance of BSTNode */
 	public BSTNode(String k, T val) {
 		key = k;
-		data = new Node<T>(val);
+		data = new DocFrequency<T>(val);
 		left = right = null;
 	}
 	
 	public BSTNode(String k, T val, BSTNode<T> l, BSTNode<T> r) {
 		key = k;
-		data = new Node<T>(val);
+		data = new DocFrequency<T>(val);
 		left = l;
 		right = r;
 	}
